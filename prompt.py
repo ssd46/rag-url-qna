ef7417c0c6c6fd09e6bebaee92dc832aea018ea2
SRI SAI DINESH Vallabhapuram <C00551763@louisiana.edu>
2025-07-27 16:51:45 -0500

    first commit

---
+++ prompt.py
@@ -0,0 +1,11 @@
+# flake8: noqa
+from langchain.prompts import PromptTemplate
+from langchain.chains.qa_with_sources.stuff_prompt import template
+
+updated_template = "You are a helpful assistant for RealEstate research." + template
+PROMPT = PromptTemplate(template=updated_template, input_variables=["summaries", "question"])
+
+EXAMPLE_PROMPT = PromptTemplate(
+    template="Content: {page_content}\nSource: {source}",
+    input_variables=["page_content", "source"],
+)
